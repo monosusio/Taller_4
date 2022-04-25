6709f1a6b0e529d08b77f716ebcc0c6572988305
unknown <ssanabriag@unbosque.edu.co>
2022-04-25 18:47:29 -0500

    si

---
+++ src/main/java/co/edu/unbosque/taller_4/LogInServlet.java
@@ -0,0 +1,59 @@
+package co.edu.unbosque.taller_4;
+
+
+import java.io.*;
+import java.util.List;
+
+import co.edu.unbosque.taller_3.DTO.User;
+import co.edu.unbosque.taller_3.services.UserService;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "login", value = "/login")
+public class LogInServlet extends HttpServlet {
+
+    public void init() {}
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String role = request.getParameter("role");
+
+        List<User> users = new UserService().getUsers();
+
+        User userFounded = users.stream()
+                .filter(user -> username.equals(user.getUsername()) && password.equals(user.getPassword()))
+                .findFirst()
+                .orElse(null);
+
+        if (userFounded != null) {
+            request.setAttribute("role", userFounded.getRole());
+
+            Cookie cookie = new Cookie("role", userFounded.getRole());
+            cookie.setMaxAge(20);
+            response.addCookie(cookie);
+
+
+
+            if(userFounded.getRole().equals("Artist")) {
+                RequestDispatcher dispatcher = request.getRequestDispatcher("./artista.html");
+                dispatcher.forward(request, response);
+
+            } else if (userFounded.getRole().equals("Customer")) {
+
+                RequestDispatcher dispatcher = request.getRequestDispatcher("./usuario.html");
+                dispatcher.forward(request, response);
+            }
+
+
+        } else {
+            response.sendRedirect("./401.html");
+        }
+    }
+
+    public void destroy() {}
+}
---
+++ src/main/java/co/edu/unbosque/taller_4/SignUpServlet.java
@@ -0,0 +1,34 @@
+package co.edu.unbosque.taller_4;
+
+import co.edu.unbosque.taller_3.services.UserService;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.File;
+import java.io.IOException;
+
+@WebServlet(name = "signup", value = "/signup")
+public class SignUpServlet extends HttpServlet {
+
+    public void init() {}
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String role = request.getParameter("role");
+
+
+        try {
+            new UserService().createUser(username, password, role, getServletContext().getRealPath("") + File.separator);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        response.sendRedirect("./index.html");
+    }
+
+    public void destroy() {}
+}
